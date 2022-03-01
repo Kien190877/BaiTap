613158d49285bef71cd0947d39832874b76e412f
Q.Kien <traquangkien190877@gmail.com>
2022-03-02 00:42:49 +0700

    update

--- src/Ngay23Feb.java
+++ src/Ngay23Feb.java
@@ -1,5 +1,15 @@
 public class Ngay23Feb {
     public static void  main(String[] args){
+        // 1. kieu du lieu nguyen thuy (premitive type)
+        // String (chuổi)
+        // int, long (số nguyên, là số không có phần thập phân)
+        // float, double (số thực, có phần thập phân)
+        // boolean (chí có 2 miền giá trị: Đúng/Sai)
+        // char (kiểu ký tự)
+
+
+
+
 
     }
 }
